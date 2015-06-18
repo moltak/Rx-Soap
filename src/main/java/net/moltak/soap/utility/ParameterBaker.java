bd6d4c48ba46c4200a047404a3edbdc3c5bb2c96
moltak <moltak@gmail.com>
2015-06-18 19:54:23 +0900

    Bug fix for jacoco reports on ParameterBaker.java

--- src/main/java/net/moltak/soap/utility/ParameterBaker.java
+++ src/main/java/net/moltak/soap/utility/ParameterBaker.java
@@ -12,31 +12,34 @@ import java.util.Map;
  * Created by moltak on 15. 2. 5..
  */
 public class ParameterBaker {
-    public static Map<String, String> bake(BaseRequest caller)
+    public static Map<String, String> bake(BaseRequest baseRequest)
             throws InvocationTargetException, IllegalAccessException {
         
         Map<String, String> map = new HashMap<>();
-        Field[]fields = caller.getClass().getDeclaredFields();
+        Field[]fields = baseRequest.getClass().getDeclaredFields();
         for(Field i : fields) {
             if("request_name".equals(i.getName())) continue;
-            
-            map.put(i.getName().toUpperCase(), runGetter(caller, i.getName()));
+
+            String getFieldValueFunction = runGetter(baseRequest, i.getName());
+            if(!getFieldValueFunction.isEmpty()) map.put(i.getName().toUpperCase(), getFieldValueFunction);
         }
         return map;
     }
 
-    private static String runGetter(BaseRequest caller, String fieldName)
+    private static String runGetter(BaseRequest baseRequest, String fieldName)
             throws InvocationTargetException, IllegalAccessException {
 
-        for(Method i : caller.getClass().getMethods()) {
+        for(Method i : baseRequest.getClass().getMethods()) {
             if (!i.getName().startsWith("get")) continue;
             if (i.getName().length() != (fieldName.length() + 3)) continue;
 
             if (i.getName().toLowerCase().endsWith(fieldName.toLowerCase())) {
-                return (String)i.invoke(caller);
+                return (String)i.invoke(baseRequest);
             }
         }
 
+        // for jacoco field.
+        if ("$jacocoData".equals(fieldName)) return "";
         throw new IllegalAccessException(String.format("There is no %s field!", fieldName));
     }
 }
